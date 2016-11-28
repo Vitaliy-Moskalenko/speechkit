b1c56b82e2e9ac34bb6b8b5e30b1ffea1b477f21
Vitaliy Moskalenko <to-@mail.ru>
2016-11-28 18:48:38 +0400

    3

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
-<?php
+﻿<?php
+
+	error_reporting(E_ALL);
+
+	include_once('inc/header.php');	
+	include_once('yandexThemAll.php');
+	
+	
+	
 
 	if(isset($_POST['send']) && $_POST['send'] == 1) {
 		
+		$dateStamp = date('ymd');
 		$stderr = fopen('errlog.txt', 'w');
 		
-		$filename = basename($_FILES['myfile']['name']);
+		$filename = basename($_FILES['myfile']['name']);		
 		$uploadfile = "upload/".$filename;
 		
-		if(copy($_FILES['myfile']['name'], $uploadfile)) echo '<h3 style="color:green;">Файл успешно загружен на сервер</h3>';
+		if(copy($_FILES['myfile']['tmp_name'], $uploadfile)) echo '<h3 style="color:green;">Файл успешно загружен на сервер</h3>';
         else { echo '<h3 style="color:red;">Ошибка! Не удалось загрузить файл на сервер!</h3>'; exit; }
 		
-		$filepath = substr().$uploadfile;
+		$filepath = substr($_SERVER['SCRIPT_FILENAME'], 0, 22).$uploadfile;
+		
+		echo "<h2>$filepath</h2>";
 		
 		$postdata = array("project_name" => "test_project_1".$dateStamp,         
 	                       "source_lang" => $_POST['src'],   
-						//   "target_lang" => $_POST['dst'],
+					//   "target_lang" => $_POST['dst'],
 						   "upload" => "@".$filepath);
 		
 	}
@@ -30,12 +41,6 @@
 	 
 ?>
 
-<!DOCTYPE html>
-<head>
-	<meta charset="utf-8" />
-	<!--  <script type="text/javascript" src="js/jquery.min.js"></script> -->
-	<link rel="stylesheet" type="text/css" href="css/large.css" /> 
-<head><body>
 
 	<div id="page">
 
@@ -44,7 +49,7 @@
 		    <input type="file" name="myfile" id="myFile" size="40" /><br/><br/>
 		    <input type="hidden" name="project_name" value="my_test_project_1" />
 			<input type="hidden" name="source_lang" value="en-US"  />
-			<input type="hidden" name="target_lang" value="ru-RU"/> 
+		<!--	<input type="hidden" name="target_lang" value="ru-RU"/>  -->
 	 		<input type="submit" value="Распознать файл" />	
             <div class="langs">Язык файла для распознавания
 				<select name="src">
